b92724da03597b4fd57b4c7fca9b8840c3fb428f
Rachel <racheldh@iastate.edu>
2025-05-02 18:22:38 -0500

    added src/aws-exports.js to config user pool

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    "aws_project_region": "us-east-2",
+    "aws_cognito_region": "us-east-2",
+    "aws_user_pools_id": "us-east-2_mW0PLfupS",
+    "aws_user_pools_web_client_id": "3adk7019ch3pupuqrs3anbkp5j"
+}
+
+export default awsmobile;
