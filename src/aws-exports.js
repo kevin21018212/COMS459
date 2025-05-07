ed2b124ddd59e69b5c8ce6b6a9bad023674e0497
Matthew Bennett <trashlinkmain@gmail.com>
2025-05-06 23:20:53 -0500

    work with manas changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,8 @@
 const awsExports = {
-    "aws_project_region": "us-east-2",
-    "aws_cognito_region": "us-east-2",
-    "aws_user_pools_id": "us-east-2_mW0PLfupS",
-    "aws_user_pools_web_client_id": "3adk7019ch3pupuqrs3anbkp5j"
-}
+  aws_project_region: "us-east-2",
+  aws_cognito_region: "us-east-2",
+  aws_user_pools_id: "us-east-2_mW0PLfupS",
+  aws_user_pools_web_client_id: "2435gfb9jjqg639vettkgb1mjo",
+};
 
 export default awsExports;
