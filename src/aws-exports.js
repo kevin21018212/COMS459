1978a4d8996b9849a13f6950cdd706ee5c648dfe
Matthew Bennett <trashlinkmain@gmail.com>
2025-05-06 23:32:36 -0500

    cog fix

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,7 +2,7 @@ const awsExports = {
   aws_project_region: "us-east-2",
   aws_cognito_region: "us-east-2",
   aws_user_pools_id: "us-east-2_mW0PLfupS",
-  aws_user_pools_web_client_id: "2435gfb9jjqg639vettkgb1mjo",
+  aws_user_pools_web_client_id: "243sfgb9ijgq639vettkgb1mjo",
 };
 
 export default awsExports;
