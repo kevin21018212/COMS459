6971c37a90874d523f0ef3af9e411ab2080c6c00
Matthew Bennett <trashlinkmain@gmail.com>
2025-05-08 14:57:21 -0500

    css changes

--- src/imageGrid.tsx
+++ src/imageGrid.tsx
@@ -45,8 +45,8 @@ export default function ImageGrid({ images, onMakePublic, onMakePrivate, onDelet
                 <button onClick={() => onMakePrivate(img.key)} disabled={loading}>
                   Make Private
                 </button>
-                <a className="link" href={img.url} target="_blank" rel="noopener noreferrer">
-                  {img.url}
+                <a className="link" href={img.url} target="_blank" rel="noopener noreferrer" title={img.url}>
+                  {img.url.length > 50 ? `${img.url.slice(0, 50)}...` : img.url}
                 </a>
               </>
             )}
